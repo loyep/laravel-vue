44a170ba1af45d62084df8c13bb1e6ba5df82574
Loren <mwl@live.com>
2019-03-12 14:33:20 +0800

    update auth bg

--- resources/grace/layouts/AuthLayout/index.vue
+++ resources/grace/layouts/AuthLayout/index.vue
@@ -12,7 +12,7 @@
       <div class="top">
         <div class="header">
           <router-link :to="{ path: '/' }">
-            <img alt="logo" class="logo" :src="logo">
+            <img alt="logo" class="logo" src="~@/assets/images/logo.png">
             <span class="title">{{ name }}</span>
           </router-link>
         </div>
@@ -43,10 +43,8 @@
 </template>
 
 <script type="text/ecmascript-6">
-import background from '@/assets/images/background.svg'
 import GlobalFooter from '@/components/GlobalFooter'
 import SelectLang from '@/components/SelectLang'
-import logo from '@/assets/images/logo.png'
 
 export default {
   name: 'AuthLayout',
@@ -57,8 +55,6 @@ export default {
   data () {
     return {
       year: new Date().getFullYear(),
-      backgroundImage: background,
-      logo,
       name: window.config.name,
       description: window.config.description
     }
