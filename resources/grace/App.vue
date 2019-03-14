da840277fcda9b0bea68cc27915a499c5c0fdf2a
Loren <mwl@live.com>
2019-03-14 14:11:51 +0800

    update loading

--- resources/grace/App.vue
+++ resources/grace/App.vue
@@ -31,6 +31,9 @@ export default {
     }
   },
   mounted () {
+    setTimeout(() => {
+      document.getElementById('grace-loader').style.display = 'none'
+    }, 200)
     deviceEnquire((screenType) => {
       this.$store.dispatch('theme/SetScreen', screenType)
     })
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,8 +10,101 @@
     <script>
       window.config = @json($config)
     </script>
+    <style>
+      #grace-loader {
+  background:#F9F9F9;
+  position:fixed;
+  width:100%;
+  height:100%;
+  left:0px;
+  top:0px;
+  z-index:99;
+}
+
+#grace-loader span{
+  width:60px;
+  height:60px;
+  position:absolute;
+  top:50%;
+  left:50%;
+  margin-top: -60px;
+  margin-left:-30px;
+  margin-right:-30px;
+  
+  -webkit-transform: rotate(45deg);
+      -ms-transform: rotate(45deg);
+          transform: rotate(45deg);
+  -webkit-animation: gRotate 1.2s infinite linear;
+          animation: gRotate 1.2s infinite linear;
+}
+
+#grace-loader i:nth-child(1) {
+  left: 0;
+  top: 0;
+}
+#grace-loader i:nth-child(2) {
+  right: 0;
+  top: 0;
+  -webkit-animation-delay: 0.4s;
+          animation-delay: 0.4s;
+}
+#grace-loader i:nth-child(3) {
+  right: 0;
+  bottom: 0;
+  -webkit-animation-delay: 0.8s;
+          animation-delay: 0.8s;
+}
+#grace-loader i:nth-child(4) {
+  left: 0;
+  bottom: 0;
+  -webkit-animation-delay: 1.2s;
+          animation-delay: 1.2s;
+}
+
+#grace-loader i {
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  background-color: #1890ff;
+  -webkit-transform: scale(0.75);
+      -ms-transform: scale(0.75);
+          transform: scale(0.75);
+  display: block;
+  position: absolute;
+  opacity: 0.3;
+  -webkit-animation: spinMove 1s infinite linear alternate;
+          animation: spinMove 1s infinite linear alternate;
+  -webkit-transform-origin: 50% 50%;
+      -ms-transform-origin: 50% 50%;
+          transform-origin: 50% 50%;
+}
+
+@-webkit-keyframes spinMove {
+  to {
+    opacity: 1;
+  }
+}
+@keyframes spinMove {
+  to {
+    opacity: 1;
+  }
+}
+@-webkit-keyframes gRotate {
+  to {
+    -webkit-transform: rotate(405deg);
+            transform: rotate(405deg);
+  }
+}
+@keyframes gRotate {
+  to {
+    -webkit-transform: rotate(405deg);
+            transform: rotate(405deg);
+  }
+}      </style>
 </head>
 <body>
+<div id="grace-loader"><span class="ant-spin-dot2 ant-spin-dot-spin2"><i></i><i></i><i></i><i></i></span>
+</div>
 <div id="app"></div>
 <script src="{{ mix('admin.js', 'assets/admin')}}" type="text/javascript"></script>
 </body>
