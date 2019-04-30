4294e613948d588eeb8c4acbb18bf69fba133b79
Loren <mwl@live.com>
2019-04-30 18:16:55 +0800

    update prism

---
+++ prism/core/database/migrations/2019_01_01_000000_create_posts_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->unsignedBigInteger('category_id')->index();
+            $table->text('keywords')->nullable();
+            $table->text('description')->nullable();
+            $table->text('excerpt')->nullable();
+            $table->unsignedSmallInteger('order')->default(1);
+            $table->unsignedBigInteger('views')->default(0);
+            $table->unsignedBigInteger('likes')->default(0);
+            $table->string('image')->nullable();
+            $table->string('type')->default('post');
+            $table->boolean('is_top')->default(false);
+            $table->enum('status', ['published', 'draft', 'private'])->default('published');
+            $table->boolean('allow_comment')->default(true);
+            $table->timestamp('published_at')->nullable()->useCurrent();
+            $table->timestamp('viewed_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
