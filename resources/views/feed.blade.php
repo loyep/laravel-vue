aa25ed2b79faa241fbd762ab1ec1896810d6aeee
Loren Liu <mwl@live.com>
2019-05-05 14:07:17 +0800

    更新 feed

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -5,7 +5,7 @@
         <link>{{ url('/') }}</link>
         <atom:link href="{{ route('feed') }}" rel="self" type="application/rss+xml"/>
         <?php
-        $date = !empty($posts) ? $posts[0]->updated_at->format('D, d M Y H:i:s O') : date("D, d M Y H:i:s O", time())
+        $date = !empty($posts) ? $posts[0]->published_at->format('D, d M Y H:i:s O') : date("D, d M Y H:i:s O", time())
         ?>
         <pubDate>{{ $date }}</pubDate>
         <lastBuildDate>{{ $date }}</lastBuildDate>
@@ -15,7 +15,7 @@
                 <title>{{ $post->title }}</title>
                 <link>{{ route('post.show',$post->slug) }}</link>
                 <description>{{ $post->description }}</description>
-                <pubDate>{{ $post->created_at->format('D, d M Y H:i:s T') }}</pubDate>
+                <pubDate>{{ $post->published_at->format('D, d M Y H:i:s T') }}</pubDate>
                 <author>{{ $post->user->email }} ({{$post->user->display_name}})</author>
                 <guid>{{ route('post.show',$post->slug) }}</guid>
                 <category>{{ $post->category->name }}</category>
